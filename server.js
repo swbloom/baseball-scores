266a023e93e180e3fc5eb40a2977cc91b359d768
Simon Bloom <swbloom@gmail.com>
2016-04-18 15:47:22 -0400

    adding custom attachment

--- server.js
+++ server.js
@@ -34,30 +34,18 @@ router.route('/scores')
   .post(function(req,res) {
     var scores = fs.readFile('scores.json', 'utf-8', (err, data) => {
       var content = JSON.parse(data);
+      var attachments = [];
+
+      data.map((game, i) => {
+        attachments.push({
+          "fallback": `${game.awayTeam.name} at ${game.homeTeam.name}`
+          "color": "#36a64f",
+          "title": `${game.awayTeam.name} at ${game.homeTeam.name}`
+        });
+      });
       res.json({
         "text": "success!",
-        "attachments": [
-                {
-                    "fallback": "Required plain-text summary of the attachment.",
-                    "color": "#36a64f",
-                    "pretext": "Optional text that appears above the attachment block",
-                    "author_name": "Bobby Tables",
-                    "author_link": "http://flickr.com/bobby/",
-                    "author_icon": "http://flickr.com/icons/bobby.jpg",
-                    "title": "Slack API Documentation",
-                    "title_link": "https://api.slack.com/",
-                    "text": "Optional text that appears within the attachment",
-                    "fields": [
-                        {
-                            "title": "Priority",
-                            "value": "High",
-                            "short": false
-                        }
-                    ],
-                    "image_url": "http://my-website.com/path/to/image.jpg",
-                    "thumb_url": "http://example.com/path/to/thumb.png"
-                }
-            ]
+        "attachments": attachments
       });
     });
   }
